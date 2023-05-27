b0d0892462c9cb82b4e17eb3ec834579a618f62a
dima <rubetsdima100@gmail.com>
2023-05-27 08:04:54 +0300

    new commit

--- MyHouse24/settings.py
+++ MyHouse24/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -173,3 +175,5 @@ EMAIL_PORT = 587
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn MyHouse24.wsgi:application --log-file -
